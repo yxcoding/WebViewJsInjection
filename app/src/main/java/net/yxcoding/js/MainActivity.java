086f132816a33999e10f80847cc5a83ebc73c4c4
yaxiongfang <598493431@qq.com>
2016-07-23 15:19:12 +0800

    #

--- app/src/main/java/net/yxcoding/js/MainActivity.java
+++ app/src/main/java/net/yxcoding/js/MainActivity.java
@@ -1,15 +1,45 @@
 package net.yxcoding.js;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity
 {
+    private WebView webView;
+
+    private String js;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebClient());
+
+        webView.loadUrl("file:///android_asset/test.html");
+
+        js = "javascript:function setValue(){x=document.getElementById(\"content\");  x.innerHTML=\"js 注入成功且方法调用成功\";}";
+    }
+
+    class WebClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url)
+        {
+
+            view.loadUrl("javascript:" + js);
+            view.loadUrl("javascript:" + "setValue()");
+
+            super.onPageFinished(view, url);
+        }
     }
 }
